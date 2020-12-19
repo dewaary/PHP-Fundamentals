d84342ea74c54e7071e42bde6b7611b36a376522
dewa.g.ari28 <dewa.g.ari28@gmail.com>
2020-12-19 18:33:26 +0700

    Operation Assigment

---
+++ .history/OperationAssigment_20201219183000.php
@@ -0,0 +1,14 @@
+<?php 
+
+$total = 10;
+
+$fruit = 5000;
+$chicken = 10000;
+$orangeJuice = 4000;
+
+
+$total += $fruit;
+$total += $chicken;
+$total += $orangeJuice;
+
+?>
---
+++ OperationAssigment.php
@@ -0,0 +1,30 @@
+<?php 
+
+// Operation Addtion (+)
+$total = 500;
+
+$fruit = 5000;
+$chicken = 10000;
+$orangeJuice = 4000;
+
+
+$total += $fruit;
+$total += $chicken;
+$total += $orangeJuice;
+
+var_dump($total);
+
+// Operation Subtrction (-)
+$total1 = 500;
+
+$fruit = 5000;
+$chicken = 10000;
+$orangeJuice = 4000;
+
+
+$total1 -= $fruit;
+$total1 -= $chicken;
+$total1 -= $orangeJuice;
+
+var_dump($total1);
+?>
